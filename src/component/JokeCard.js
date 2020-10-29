0d63233d733f183fbed52fc5c29bc991ecbc75c8
joe-mgl <otepmigue@gmail.com>
2020-10-28 19:06:25 -0700

    updating jokecard and adding jokecard.css

--- src/component/JokeCard.js
+++ src/component/JokeCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './JokeCard.css'
+
 const JokeCard = ({ randomJoke }) => {
 	return(
 		<div className='mw8 tc center br3 pa3 pa4-ns mv3 ba b--black-40'> 
-			<img className='h4 w4' alt='chuck' 
+			<img className='h3 w3' alt='chuck' 
 				 src='https://assets.chucknorris.host/img/avatar/chuck-norris.png' 
 			/>
 			<div>
-				<p className='lh-copy measure center white f1 black-70'>{randomJoke}</p>
+				<p className='flex lh-copy measure center white f2 black-60'>{randomJoke}</p>
 			</div>
 		</div>
 	)
